1ac907f2767fa02885dd59290e768c2c65a40ae6
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-08-12 22:33:43 +0900

    Initial commit

---
+++ subprocrunner/_error.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+class EmptyCommandError(Exception):
+    pass
+
+
+class CommandNotFoundError(Exception):
+    pass
