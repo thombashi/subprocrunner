c5366fe3e320210e3f2dd02d8588d1a93a236663
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-02-26 10:10:35 +0900

    [ci skip] Update docstring

--- subprocrunner/_logger.py
+++ subprocrunner/_logger.py
@@ -23,13 +23,13 @@ def set_logger(is_enable):
 
 def set_log_level(log_level):
     """
-    Set logging level of this module. The module using
+    Set logging level of this module. Using
     `logbook <http://logbook.readthedocs.io/en/stable/>`__ module for logging.
 
     :param int log_level:
-        One of the log level of the
+        One of the log level of
         `logbook <http://logbook.readthedocs.io/en/stable/api/base.html>`__.
-        Disabled logging if the ``log_level`` is ``logbook.NOTSET``.
+        Disabled logging if ``log_level`` is ``logbook.NOTSET``.
     """
 
     if log_level == logbook.NOTSET:
