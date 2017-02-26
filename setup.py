a83e545e63e4c3705122aa5d97d87e83d2363bf6
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-02-26 22:06:42 +0900

    Bump version to 0.4.11

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name=MODULE_NAME,
-    version="0.4.10",
+    version="0.4.11",
     author="Tsuyoshi Hombashi",
     author_email="gogogo.vm@gmail.com",
     url="https://github.com/thombashi/{:s}".format(MODULE_NAME),
