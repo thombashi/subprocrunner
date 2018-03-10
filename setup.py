6d75b8e48089e46398165a697c81e3270fe62d91
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-03-11 08:38:04 +0900

    Add release command

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ import sys
 
 import setuptools
 
-
 MODULE_NAME = "subprocrunner"
 REQUIREMENT_DIR = "requirements"
 
@@ -19,6 +18,25 @@ pkg_info = {}
 
 with open(os.path.join(MODULE_NAME, "__version__.py")) as f:
     exec(f.read(), pkg_info)
+
+
+class ReleaseCommand(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        tag = "v{}".format(pkg_info["__version__"])
+
+        print("Pushing git tags: {}".format(tag))
+
+        os.system("git tag {}".format(tag))
+        os.system("git push --tags")
+
 
 with open("README.rst") as fp:
     long_description = fp.read()
@@ -73,4 +91,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ])
+    ],
+    cmdclass={
+        "release": ReleaseCommand,
+    })
