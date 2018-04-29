62740cf436133017f1c8f249ba9fadb183c5c836
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-29 14:16:01 +0900

    Rename a constant variable

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
     docs_requires = [line.strip() for line in f if line.strip()]
 
-pytest_runner = ["pytest-runner"] if need_pytest() else []
+PYTEST_RUNNER_REQUIRES = ["pytest-runner"] if need_pytest() else []
 
 setuptools.setup(
     name=MODULE_NAME,
@@ -75,7 +75,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["test*"]),
 
     install_requires=install_requires,
-    setup_requires=pytest_runner,
+    setup_requires=PYTEST_RUNNER_REQUIRES,
     tests_require=tests_requires,
     extras_require={
         "build": "wheel",
