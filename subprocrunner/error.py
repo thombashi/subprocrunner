642c6f9402b2d1c36698211f1f4d58264e25af86
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-09-02 17:47:20 +0900

    Add an error class compatible with subprocess module of Python 3.5 or later

--- subprocrunner/error.py
+++ subprocrunner/error.py
@@ -6,6 +6,9 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import subprocess
+import sys
+
 
 class CommandError(Exception):
     @property
@@ -21,6 +24,16 @@ class CommandError(Exception):
         self.__errno = kwargs.pop("errno", None)
 
         super(CommandError, self).__init__(*args, **kwargs)
+
+
+class CalledProcessError(subprocess.CalledProcessError):
+    def __init__(self, *args, **kwargs):
+        if sys.version_info[0:2] <= (3, 4):
+            # stdout and stderr attribute added to subprocess.CalledProcessError since Python 3.5
+            self.stdout = kwargs.pop("stdout", None)
+            self.stderr = kwargs.pop("stderr", None)
+
+        super(CalledProcessError, self).__init__(*args, **kwargs)
 
 
 class InvalidCommandError(CommandError):
