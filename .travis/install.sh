84da6f90fbe67a9f9449dcdac2cdb897798ff973
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-12-24 15:13:41 +0900

    Fix package installation for CI

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
         brew upgrade python
     fi
 
-    pip3 install setuptools --upgrade
+    pip3 install setuptools tox --upgrade
 else
-    pip install setuptools --upgrade
+    pip install setuptools tox --upgrade
 fi
