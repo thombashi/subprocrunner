5542730c1c024d14d86371350424e8f0a1437014
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-04-30 18:50:31 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -71,15 +71,15 @@ setuptools.setup(
         "Tracker": "{:s}/issues".format(REPOSITORY_URL),
     },
 
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*",
     install_requires=SETUPTOOLS_REQUIRES + install_requires,
     setup_requires=SETUPTOOLS_REQUIRES + PYTEST_RUNNER_REQUIRES,
     tests_require=tests_requires,
     extras_require={
-        "build": ["wheel"],
+        "build": ["twine", "wheel"],
         "docs": docs_requires,
         "logging": ["Logbook>=1.1.0,<2.0.0"],
-        "release": ["releasecmd>=0.0.12,<0.1.0"],
+        "release": ["releasecmd>=0.0.18,<0.1.0"],
         "test": tests_requires,
     },
 
