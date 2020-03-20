bb42028776452e41d220ce17509e9c6cf2289343
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-03-20 23:13:48 +0900

    Add follow_symlinks argument to Which constructor

--- subprocrunner/_which.py
+++ subprocrunner/_which.py
@@ -2,8 +2,8 @@
 .. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
 """
 
-
 import errno
+import os
 import shutil
 from typing import Optional
 
@@ -15,15 +15,20 @@ class Which:
     def command(self):
         return self.__command
 
-    def __init__(self, command: str) -> None:
+    def __init__(self, command: str, follow_symlinks: bool = False) -> None:
         if not command:
             raise ValueError("require a command")
 
         self.__command = command
+        self.__follow_symlinks = follow_symlinks
         self.__abspath = None  # type: Optional[str]
 
     def __repr__(self) -> str:
-        item_list = ["command={}".format(self.command), "is_exist={}".format(self.is_exist())]
+        item_list = [
+            "command={}".format(self.command),
+            "is_exist={}".format(self.is_exist()),
+            "follow_symlinks={}".format(self.__follow_symlinks),
+        ]
 
         if self.is_exist():
             item_list.append("abspath={}".format(self.abspath()))
@@ -44,5 +49,10 @@ class Which:
             return self.__abspath
 
         self.__abspath = shutil.which(self.command)
+        if self.__abspath is None:
+            return self.__abspath
+
+        if self.__follow_symlinks and os.path.islink(self.__abspath):
+            self.__abspath = os.path.realpath(self.__abspath)
 
         return self.__abspath
