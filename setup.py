3b44bd3e825cf14ebcbe9aecf63db4e34f31ebe6
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-04-06 22:18:26 +0900

    Drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setuptools.setup(
         "Source": REPOSITORY_URL,
         "Tracker": f"{REPOSITORY_URL:s}/issues",
     },
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=install_requires,
     extras_require={"logging": LOGGING_REQUIRES, "test": tests_requires + LOGGING_REQUIRES},
     classifiers=[
@@ -72,7 +72,6 @@ setuptools.setup(
         "Operating System :: POSIX",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
